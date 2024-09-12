3fa36f1e36d710d2daa66cf0c0834b1d87909e60
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-09-12 21:24:18 +0300

    Added text updating system.

--- kernel.cpp
+++ kernel.cpp
@@ -28,35 +28,46 @@ char* itoa(int value, char* str, int base) {
         char tmp = *low;
         *low++ = *ptr;
         *ptr-- = tmp;
-    }
-
-    return rc;
+    } return rc;
 }
 
-uint64_t cursor_position = 0;
+uint64_t cursorPos = 0;
+
 void print(const char* message, uint8_t color = 0x00 | 0x0F) {
     for (uint64_t i = 0; 1; i++) {
         if (message[i] == '\0') return;
         else if (message[i] == '\n') {
-            cursor_position += ROW_SIZE - (cursor_position % ROW_SIZE);
+            cursorPos += ROW_SIZE - (cursorPos % ROW_SIZE);
             continue;
         }
 
-        *(uint8_t*) (VIDEO_MEMORY + cursor_position * 2) = message[i];
-        *(uint8_t*) (VIDEO_MEMORY + (cursor_position * 2) + 1) = color;
-        cursor_position++;
+        *(uint8_t*) (VIDEO_MEMORY + cursorPos * 2) = message[i];
+        *(uint8_t*) (VIDEO_MEMORY + (cursorPos * 2) + 1) = color;
+        cursorPos++;
     }
 }
 
 void clear(uint8_t color = 0x00 | 0x00) {
+    cursorPos = 0;
+
     for (uint64_t i = 0; i < ROW_SIZE * COL_SIZE; i++) {
         *(uint8_t*) (VIDEO_MEMORY + i * 2) = 219;
         *(uint8_t*) (VIDEO_MEMORY + (i * 2) + 1) = color;
     }
 }
 
+void wait(uint64_t ticks) {
+    uint64_t start = 0;
+    while (start < ticks) start++;
+}
+
 extern "C" void _start() {
-    print("sgsdg\n");
-    print(itoa(199, nullptr, 10));
-    return;
+    int x = 0;
+
+    while (true) {
+        clear();
+        print("Hello, World!\nTicks: ");
+        print(itoa(x++, nullptr, 10));
+        wait(100000000);
+    } return;
 }

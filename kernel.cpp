03de72792dbc34297a15f1bfae61310d24ba45fe
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-09-13 21:11:45 +0300

    Added buffer swapping and panic mode.

--- kernel.cpp
+++ kernel.cpp
@@ -31,43 +31,62 @@ char* itoa(int value, char* str, int base) {
     } return rc;
 }
 
+uint8_t videoBuffer[ROW_SIZE * COL_SIZE * 2];
 uint64_t cursorPos = 0;
 
-void print(const char* message, uint8_t color = 0x00 | 0x0F) {
-    for (uint64_t i = 0; 1; i++) {
-        if (message[i] == '\0') return;
-        else if (message[i] == '\n') {
+void EnterPanicMode(const char* reason);
+
+void SetCharAt(uint64_t pos, uint8_t chr, uint8_t color = 0x00 | 0x0F) {
+    if (pos >= ROW_SIZE * COL_SIZE)
+        EnterPanicMode("Cursor position out of bounds.");
+
+    videoBuffer[pos * 2] = chr;
+    videoBuffer[pos * 2 + 1] = color;
+}
+
+void WriteString(const char* string, uint8_t color = 0x00 | 0x0F) {
+    for (uint64_t i = 0; string[i] != '\0'; i++) {
+        if (string[i] == '\n') {
             cursorPos += ROW_SIZE - (cursorPos % ROW_SIZE);
-            continue;
+        } else {
+            SetCharAt(cursorPos++, string[i], color);
         }
-
-        *(uint8_t*) (VIDEO_MEMORY + cursorPos * 2) = message[i];
-        *(uint8_t*) (VIDEO_MEMORY + (cursorPos * 2) + 1) = color;
-        cursorPos++;
     }
 }
 
-void clear(uint8_t color = 0x00 | 0x00) {
-    cursorPos = 0;
-
+void Clear(uint8_t color = 0x00 | 0x00) {
     for (uint64_t i = 0; i < ROW_SIZE * COL_SIZE; i++) {
-        *(uint8_t*) (VIDEO_MEMORY + i * 2) = 219;
-        *(uint8_t*) (VIDEO_MEMORY + (i * 2) + 1) = color;
-    }
+        SetCharAt(i, 219, color);
+    } cursorPos = 0;
 }
 
-void wait(uint64_t ticks) {
-    uint64_t start = 0;
-    while (start < ticks) start++;
+void SwapBuffers() {
+    for (uint64_t i = 0; i < ROW_SIZE * COL_SIZE * 2; i++)
+        *(uint8_t*) (VIDEO_MEMORY + i) = videoBuffer[i];
+}
+
+void EnterPanicMode(const char* reason) {
+    while (true) {
+        Clear(0x00 | 0x04);
+        WriteString("Kernel panic occured, system halted.\n", 0x00 | 0x04);
+        WriteString("Reason: ", 0x00 | 0x04);
+        WriteString(reason, 0x00 | 0x04);
+        SwapBuffers();
+    }
 }
 
 extern "C" void _start() {
-    int x = 0;
+    uint64_t x = 0;
 
     while (true) {
-        clear();
-        print("Hello, World!\nTicks: ");
-        print(itoa(x++, nullptr, 10));
-        wait(100000000);
-    } return;
+        Clear();
+        WriteString("Hello, World!\nTicks: ");
+        WriteString(itoa(x++, nullptr, 10));
+        WriteString("\n");
+
+        for (uint64_t i = 0; i < (uint64_t) (x / 100); i++)
+            WriteString("Wow!\n"); // This is just to test the panic mode.
+
+        SwapBuffers();
+    }
 }

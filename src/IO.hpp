b95fd000d7d77896ed32ab9a73759c8f4314e7d0
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-09-16 16:27:21 +0300

    Added RemapPIC function.

---
+++ src/IO.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <stdint.h>
+
+#define PIC1_COMMAND 0x20
+#define PIC1_DATA 0x21
+#define PIC2_COMMAND 0xA0
+#define PIC2_DATA 0xA1
+
+#define ICW1_INIT 0x10
+#define ICW1_ICW4 0x01
+#define ICW4_8086 0x01
+
+void outb(uint16_t port, uint8_t data) {
+    asm volatile ("outb %0, %1" : : "a" (data), "Nd" (port));
+}
+
+uint8_t inb(uint16_t port) {
+    uint8_t data;
+    asm volatile ("inb %1, %0" : "=a" (data) : "Nd" (port));
+    return data;
+}
+
+void RemapPIC() {
+    uint8_t a1 = inb(PIC1_DATA);
+    uint8_t a2 = inb(PIC2_DATA);
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    outb(PIC1_DATA, 0);
+    outb(PIC2_DATA, 8);
+    outb(PIC1_DATA, 4);
+    outb(PIC2_DATA, 2);
+    outb(PIC1_DATA, ICW4_8086);
+    outb(PIC2_DATA, ICW4_8086);
+    outb(PIC1_DATA, a1);
+    outb(PIC2_DATA, a2);
+}
